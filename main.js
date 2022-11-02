70911e600f80cad2b5729395523adb50740830d2
joao pedro <87543850+jaoopedro@users.noreply.github.com>
2022-11-02 18:25:19 -0300

    Foi acrecentado funções logicas do teclado e utilizado também operadores logicos como o operado  OU '||'

--- main.js
+++ main.js
@@ -2,29 +2,34 @@ function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
-//document.querySelector('.tecla_pom').onclick = tocaSomPom; // foi criado um filtro com o querySelector na lecla especificada e passado a função do aufio atraves do onclick
-
-//Criando referencias
 //com o 'const' determonamos o valor/variavel constante ( fundamento para lidar com listas)
 
 const listadeTeclas = document.querySelectorAll('.tecla'); // o queryselectorAll serve para selecionar todos os elementos de um class ou tag especifica
 
-//enquanto == while
 for (let contador = 0; contador < listadeTeclas.length; contador++){   
     const tecla = listadeTeclas[contador]
     
     const instrumento = tecla.classList[1];
                    
-    const idAudio = `#som_${instrumento}`//template string
+    const idAudio = `#som_${instrumento}` //template string
 
-    console.log(idAudio)
-
-                     //Funcão anonima E essas funções sem nome só podem ser utilizadas nesse contexto de quando elas são o valor de algum atributo ou estão sendo armazenadas dentro de alguma referência constante ou variável.
-    tecla.onclick = function(){
+    tecla.onclick = function(){ //Funcão anonima E essas funções sem nome só podem ser utilizadas nesse contexto de quando elas são o valor de algum atributo ou estão sendo armazenadas dentro de alguma referência constante ou variável.
+    
         tocaSom(idAudio)
+        
     }
 
-    //contador = contador + 1 foi colocado dentro do parametro for 'contador++'
+    tecla.onkeydown = function(evento){  //onkeydown é um evento para quando cliar em uma tecla no caso a tecla estiver abaixada
+                                //OU
+        if (evento.code==='Space' || evento.code === 'NumpadEnter') {
+            tecla.classList.add('ativa');
+        }
 
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa')
+    }
+  
  } 
  
